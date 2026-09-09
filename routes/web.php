b6a6960c8b0d7d3b61bb26a5d13fe7ec540412ac
Andy <andy.wicaksono@ciputra.ac.id>
2026-09-09 09:44:53 +0700

    feat: route to post and user

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', function () {
+    return 'Ini halaman post';
+});
+
+Route::get('/user', function () {
+    return 'Ini halaman user';
+});
