bd46f760fda111ae4cd9f98b31960535ff0919ab
Andy <andy.wicaksono@ciputra.ac.id>
2026-09-14 16:11:24 +0700

    fix: soft deletes on post and comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['post_id', 'user_id', 'content'])]
 class Comment extends Model
 {
+    use SoftDeletes;
     //
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['user_id', 'content', 'image'])]
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     //
 }
